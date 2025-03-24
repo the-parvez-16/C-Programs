01516c2073a62326e4a32693ebe56de820626afa
the-parvez-16 <parvez.k.m.16@gmail.com>
2025-03-24 17:00:00 +0530

    HackerRank

---
+++ HackerRank/31-Bitwise_Minimum _Without_ Comparison _Operators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(((a-b)>>31)&1){
+        printf("%d",a);
+    }
+    else
+        printf("%d ",b);
+    return 0;
+}
